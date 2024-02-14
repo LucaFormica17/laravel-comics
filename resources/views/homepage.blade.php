be9ce286aafd3e02cf54a11c845205c3e84a8fce
LucaFormica17 <145986784+LucaFormica17@users.noreply.github.com>
2024-02-14 10:44:42 +0100

    [UPDATE] create layout and extends in homepage

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,46 +1,54 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>DC Comics</title>
-
-    <!-- Fonts -->
-    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-
-    <!-- Styles -->
-    @vite('resources/js/app.js')
-
-</head>
-    @include('partials/header')
-    <body>
-        <main>
-            <div class="jumbotron">
-                <div class="current">
-                    current series
-                </div>  
+@extends('layout.app')
+@section('content')    
+    <div class="jumbotron">
+        <div class="current">
+            current series
+        </div>  
+    </div>
+    <div class="bg-dark">
+        <div class="container">
+            <div class="row py-5">
+                @foreach ($comics as $comic)
+                <div class="col-2">
+                    <div class="square-img">
+                        <img src="{{$comic['thumb']}}" alt="{{$comic['title']}}">
+                        <span class="text-white"> 
+                            {{$comic['title']}}    
+                        </span>
+                    </div>
+                </div>    
+                @endforeach
             </div>
-            <div class="bg-dark">
-                <div class="container">
-                    <div class="row py-5">
-                        @foreach ($comics as $comic)
-                            <div class="col-2">
-                                <div class="square-img">
-                                    <img src="{{$comic['thumb']}}" alt="{{$comic['title']}}">
-                                    <span class="text-white"> 
-                                        {{$comic['title']}}    
-                                    </span>
-                                </div>
-                            </div>    
-                        @endforeach
+        </div>
+    </div>
+    <div class="bg-main-blue">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="d-flex">
+                        <div class="banner-card">
+                            <img src="{{ Vite::asset('resources/img/buy-comics-digital-comics.png')}}" alt="image">
+                            <p>dc comics</p>
+                        </div>
+                        <div class="banner-card">
+                            <img src="{{ Vite::asset('resources/img/buy-comics-merchandise.png')}}" alt="image">
+                            <p>dc merchandaise</p>
+                        </div>
+                        <div class="banner-card">
+                            <img src="{{ Vite::asset('resources/img/buy-comics-subscriptions.png')}}" alt="image">
+                            <p>subscription</p>
+                        </div>
+                        <div class="banner-card">
+                            <img src="{{ Vite::asset('resources/img/buy-comics-shop-locator.png')}}" alt="image">
+                            <p>comic shop locator</p>
+                        </div>
+                        <div class="banner-card">
+                            <img src="{{ Vite::asset('resources/img/buy-dc-power-visa.svg')}}" alt="image">
+                            <p>dc power visa</p>
+                        </div>
                     </div>
                 </div>
             </div>
-        </main>
-    </body>
-    @include('partials/footer')
-</html>
+        </div>
+    </div>
+@endsection
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,34 +1,5 @@
 <footer>
-    <div class="bg-main-blue">
-        <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    <div class="d-flex">
-                        <div class="banner-card">
-                            <img src="{{ Vite::asset('resources/img/buy-comics-digital-comics.png')}}" alt="image">
-                            <p>dc comics</p>
-                        </div>
-                        <div class="banner-card">
-                            <img src="{{ Vite::asset('resources/img/buy-comics-merchandise.png')}}" alt="image">
-                            <p>dc merchandaise</p>
-                        </div>
-                        <div class="banner-card">
-                            <img src="{{ Vite::asset('resources/img/buy-comics-subscriptions.png')}}" alt="image">
-                            <p>subscription</p>
-                        </div>
-                        <div class="banner-card">
-                            <img src="{{ Vite::asset('resources/img/buy-comics-shop-locator.png')}}" alt="image">
-                            <p>comic shop locator</p>
-                        </div>
-                        <div class="banner-card">
-                            <img src="{{ Vite::asset('resources/img/buy-dc-power-visa.svg')}}" alt="image">
-                            <p>dc power visa</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    
     <div class="bg-image">
         <div class="container">
             <div class="row">
